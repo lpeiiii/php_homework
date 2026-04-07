c1df51442231dd2e535510c62cc92f4733c540ee
lpeiiii <a1133364@mail.nuk.edu.tw>
2026-04-07 16:41:48 +0800

    Delete A1133364_李佩蓁_作業2/login.php

--- A1133364_李佩蓁_作業2/login.php
+++
@@ -1,8 +0,0 @@
-<form action="logincheck.php" method="POST">
-
-    ID:<input type="text" name="uID"><br/>
-    Password:<input type="password" name="uPWD"><br/>
-    <input type ="submit">
-
-    
-</form>
