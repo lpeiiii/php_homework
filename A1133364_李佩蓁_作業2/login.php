bd640252f145578121fb4948726d4119dec386fa
lpeiiii <a1133364@mail.nuk.edu.tw>
2026-04-07 16:39:58 +0800

    Update login.php

--- A1133364_李佩蓁_作業2/login.php
+++ A1133364_李佩蓁_作業2/login.php
@@ -1,8 +1,21 @@
 <form action="logincheck.php" method="POST">
 
-    ID:<input type="text" name="uID"><br/>
-    Password:<input type="password" name="uPWD"><br/>
-    <input type ="submit">
+    <h1>夏令營報名表單登入頁面</h1>
+    <table cellpadding="10">
+        <tr>
+            <td>ID:</td>
+            <td><input type="text" name="uID"></td>
+        </tr>
+        <tr>
+            <td>Password:</td>
+            <td><input type="password" name="uPWD"></td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center">
+                <input type="submit">
+            </td>
+        </tr>
+    </table>
 
     
 </form>
