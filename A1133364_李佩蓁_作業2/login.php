7743ea45933728881e8f204b3f1ea806f298516a
lpeiiii <a1133364@mail.nuk.edu.tw>
2026-04-07 16:42:03 +0800

    Add files via upload

---
+++ A1133364_李佩蓁_作業2/login.php
@@ -0,0 +1,21 @@
+<form action="logincheck.php" method="POST">
+
+    <h1>夏令營報名表單登入頁面</h1>
+    <table cellpadding="10">
+        <tr>
+            <td>ID:</td>
+            <td><input type="text" name="uID"></td>
+        </tr>
+        <tr>
+            <td>Password:</td>
+            <td><input type="password" name="uPWD"></td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center">
+                <input type="submit">
+            </td>
+        </tr>
+    </table>
+
+    
+</form>
